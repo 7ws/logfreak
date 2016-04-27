7a1b27fe99f40cd5f127824d4f29a8f4cecd4050
Evandro Myller <eMyller@7ws.co>
2016-04-27 00:38:34 -0300

    Add basic Entry and Contact models

--- backend/base/models.py
+++ backend/base/models.py
@@ -11,3 +11,40 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class LogEntry(BaseModel):
+
+    """Representation of a log entry
+
+    This model is the base for any kind of log entry.
+    """
+
+    contact = models.ForeignKey(
+        'base.Contact',
+        editable=False,
+        null=True,
+    )
+
+
+class Contact(BaseModel):
+
+    """Representation of the contact related to a log entry
+
+    A contact might represent a person or a "thing" (e.g. an organisation).
+    """
+
+    TYPE_PERSON = 'p'
+    TYPE_THING = 't'
+
+    type = models.CharField(
+        choices=[
+            (TYPE_PERSON, 'Person'),
+            (TYPE_THING, 'Thing'),
+        ],
+        max_length=1,
+    )
+
+    name = models.CharField(
+        max_length=100,
+    )
