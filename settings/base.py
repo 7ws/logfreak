c18d0079d27803e46f5a35a2998bb4586ef68eb7
Evandro Myller <eMyller@7ws.co>
2016-04-29 02:42:04 -0300

    Set up templates properly (+Jade)

--- settings/base.py
+++ settings/base.py
@@ -45,24 +45,6 @@ MIDDLEWARE_CLASSES = [
 ]
 
 
-# Template finders and processors
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-
 # URL and gateway config
 ROOT_URLCONF = 'backend.urls'
 WSGI_APPLICATION = 'backend.wsgi.application'
--- settings/static.py
+++ settings/static.py
@@ -1,11 +1,41 @@
 from decouple import config
 
-from .base import BASE_DIR
+from .base import BASE_DIR, DEBUG
 
 
 # URL to serve static files
 STATIC_URL = config('STATIC_URL', default='/static/')
 
+# Directories to find static files
 STATICFILES_DIRS = [
     BASE_DIR.child('frontend'),
+]
+
+# Template finders and processors
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR.child('frontend', 'templates'),
+        ],
+        'OPTIONS': {
+            'debug': DEBUG,
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request'
+            ],
+            'loaders': [
+                ('pyjade.ext.django.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+
+                    # Support 3rd-party apps
+                    'django.template.loaders.app_directories.Loader',
+                ]),
+            ],
+            'builtins': ['pyjade.ext.django.templatetags'],
+        },
+    },
 ]
