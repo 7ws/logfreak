b1080294fbf472bf979f4923f574a9b8a45f0949
Evandro Myller <eMyller@7ws.co>
2016-05-02 02:14:28 -0300

    Add a structure to allow connecting to sources

--- backend/webview/views/sources.py
+++ backend/webview/views/sources.py
@@ -1,4 +1,7 @@
+from functools import lru_cache
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse as r
 from django.utils.translation import ugettext as _
 from django.views import generic
 
@@ -15,3 +18,47 @@ class UserSourcesListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return Source.objects.filter(user=self.request.user)
+
+
+class UserSourcePreCreateView(LoginRequiredMixin, generic.TemplateView):
+
+    """Let users select a provider to connect to a log source
+    """
+
+    page_title = _('Connect to a new source')
+    template_name = 'sources/pre-create.jade'
+
+    def get_context_data(self, **kwargs):
+        data = super(UserSourcePreCreateView, self).get_context_data(**kwargs)
+
+        # Build a list of links to source type-specific creation views
+        data['creation_links'] = [
+            (
+                source_model._meta.verbose_name,
+                r('sources:create_for', kwargs={'name': source_model._name})
+            )
+            for source_model in Source.__subclasses__()
+        ]
+
+        return data
+
+
+class UserSourceCreateView(LoginRequiredMixin, generic.RedirectView):
+
+    """Let users connect to a log source
+    """
+
+    @lru_cache(maxsize=None)
+    def get_model(self):
+        """Get the model according to `name` URL kwarg
+        """
+        models_map = {
+            model._name: model
+            for model in Source.__subclasses__()
+        }
+        return models_map[self.kwargs['name']]
+
+    def get_redirect_url(self, *args, **kwargs):
+        # Bridge the request to the provider connection
+        model = self.get_model()
+        return model.authorize(self.request)
