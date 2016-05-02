ebb961ec5c0d454d343605d08b122c3d09466656
Patrick Mazulo <pmazulo@gmail.com>
2016-05-01 22:38:11 -0300

    Rename field from type to _type (again)

--- backend/webview/admin/sms_logger.py
+++ backend/webview/admin/sms_logger.py
@@ -3,4 +3,4 @@ from .base import BaseModelAdmin
 
 class SMSEntryAdmin(BaseModelAdmin):
 
-    list_display = ('contact_phone', 'type', 'datetime', 'text',)
+    list_display = ('contact_phone', '_type', 'datetime', 'text',)
