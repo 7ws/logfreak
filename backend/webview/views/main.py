5701e94b3d871e742099f495451e00a1b089ef22
Evandro Myller <eMyller@7ws.co>
2016-05-02 20:47:00 -0300

    Change the Home view so that it redirects to user's log

--- backend/webview/views/main.py
+++ backend/webview/views/main.py
@@ -1,12 +1,11 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse as r
 from django.views import generic
 
 
-class HomeView(LoginRequiredMixin, generic.View):
+class HomeView(generic.RedirectView):
 
-    """A dumb home view
+    """A dumb home view that redirects to an actual one
     """
 
-    def get(self, request):
-        return HttpResponse('Welcome, {}!'.format(request.user))
+    def get_redirect_url(self):
+        return r('log:log')
